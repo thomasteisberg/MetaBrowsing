0dda7fc827ef17617a122943fc89762408c466e9
Alex Bertrand <alex.bertrand13@gmail.com>
2014-01-25 11:33:37 -0800

    Got the fucking AJAX working.  CODEPENIS.

--- graph_generate.js
+++ graph_generate.js
@@ -1,31 +1,34 @@
+getTopSiteData();
+
 function getTopSiteData() {
         //This function pulls from the 'analytics' SQLite database
         //It returns an array of sites visited and the number of times
         //they were visited, sorted descending.
-        var siteVisits = [];
+        var siteVisits = new Array();
 
         var db = openDatabase('focusHistoryDB', '1.0', 'Page Focus History', 2 * 1024 * 1024);
 
         db.transaction(function (tx) {
-                tx.executeSql("SELECT * FROM analytics ORDER BY visits DESC LIMIT 10", [], function (tx, results) {
-                        console.log("hiya");
+                tx.executeSql("SELECT * FROM analytics ORDER BY visits DESC LIMIT 3", [], function (tx, results) {
                         for(var i = 0;i < results.rows.length;i++) {
                                 var row = results.rows.item(i);
                                 var subArray = [row.visits, row.taburl];
                                 siteVisits.push(subArray);
-                                console.log(siteVisits[i]);
+                                //console.log(siteVisits[i]);
                         }
+                        console.log(siteVisits);
+                        buildGraph(siteVisits);
                 });
-        });
-
-        return siteVisits;
+                
+        }); 
 }
 
-var data = getTopSiteData();
+function buildGraph(data_copy) {
+
 
 // but actually, the data will come from a server query
 
-var data_copy = data;
+//var data_copy = data;
 
 var x = d3.scale.linear()
     .domain([0, 18])
@@ -33,7 +36,7 @@ var x = d3.scale.linear()
 
 d3.select(".sitenames")
   .selectAll("h4")
-    .data(data)
+    .data(data_copy)
   .enter().append("h4")
     .text(function(d) { return d[1]; });
 
@@ -45,3 +48,4 @@ d3.select(".chart")
     .style("width", function(d) {return x(d[0]) + "px";} )
     .text(function(d) {return d[0];});
 
+}
