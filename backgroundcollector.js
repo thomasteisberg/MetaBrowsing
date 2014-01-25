96fbb109980780a3c4f1d2af68b825fa7faac454
Thomas Teisberg <teisberg@stanford.edu>
2014-01-25 03:41:32 -0800

    Solid single-session focus tracking. Focus history displayed via button press. Updates whenever you return to the focus history tab. Configurable FOCUS_TIME ignores brief glances at a tab (such as when navigating by Ctrl-Tab).

--- backgroundcollector.js
+++ backgroundcollector.js
@@ -1,24 +1,46 @@
+// Settings
+var QUERY_FREQUENCY = 100; // How often (in ms) to query the current tab
+var FOCUS_TIME = 1000; // How long (in ms) should the user be looking at a
+										// page before it counts as them switching to it
+
 // Temporary log of timestamps and tabs visited
-historyLog = new Array();
+var historyLog = new Array();
+
+// The last URL the user was looking at
+var lastUrlSeen = "";
+// Milliseconds timestamp of when the user started looking at that URL
+var lastUrlFocusBeginning = 0;
 
 // Callback for when Chrome responds with the requested current tab
 // Checks if the active tab differs from the last active tab and
 // stores it if so
 // Also schedules the next call to getPage()
-recordPage = function (tabs) {
+var recordPage = function (tabs) {
+	// Current timestamp
+	var millis = +new Date();
+	
+	// Track when focus switched to this page
+	if(tabs[0].url != lastUrlSeen){
+		lastUrlSeen = tabs[0].url;
+		lastUrlFocusBeginning = millis;
+	}
+	
 	// Only save the current tab information if the user has changed tabs
-	if(historyLog.length == 0 || tabs[0].url != historyLog[historyLog.length - 1].taburl){
-		var unixtimestamp = Math.round(+new Date()/1000)
-		historyLog[historyLog.length] = {taburl: tabs[0].url, timestamp: unixtimestamp};
+	// and they have been on this tab for at least 2 seconds
+	if(millis - lastUrlFocusBeginning > FOCUS_TIME &&
+			(historyLog.length == 0 ||
+				tabs[0].url != historyLog[historyLog.length - 1].taburl)){
+		historyLog[historyLog.length] = {taburl: tabs[0].url, timestamp: Math.round(lastUrlFocusBeginning/1000)};
+		// Timestamp stored as a unix style timestamp
 	}
 	
 	// Schedule the next tab request
-	setTimeout(getPage, 100);
+	setTimeout(getPage, QUERY_FREQUENCY);
 }
 
 // Requests the active tab from chrome. Response is handled by
 // recordPage callback function
-getPage = function () {
+var getPage = function () {
 	chrome.tabs.query({"active": true, "lastFocusedWindow": true}, recordPage);
 }
 
@@ -32,7 +54,6 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
 });
 
 // Handle requests for the data from other parts of the extension
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
--- rawdata.js
+++ rawdata.js
@@ -1,7 +1,33 @@
+// Load JQuery (local copy)
+jq=document.createElement('script');
+jq.src='jquery-2.1.0.js';
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+// Response function that displays data sent from the background page
 processData = function (response) {
-	for(var p = 0; p < response.historyLog.length; p++){
-		document.write(response.historyLog[p].taburl + "<br />");
-	}
+		$('#history-container').empty(); // Clear the old listing
+		for(var p = 0; p < response.historyLog.length; p++){
+			$('#history-container').append(
+				'<div class="entry"><div class="timestamp">'
+					+ response.historyLog[p].timestamp + '</div>'
+					+ response.historyLog[p].taburl + "</div>");
+		}
+	};
+
+// Wait for the window to be loaded so we know jQuery is available
+window.onload = function() {
+	 
+	// Create a container for the history listig
+	$('body').append('<div id="history-container"></div>');
+	 
+	// On page load request data
+	$(document).ready(function() {
+		  chrome.runtime.sendMessage({type: "currDataReq"}, processData);
+	}); 
+	// Also request data on page focus (i.e. when you return to this tab)
+	$(window).focus(function() {
+			console.log("focus");
+		  chrome.runtime.sendMessage({type: "currDataReq"}, processData);
+	}); 
+
 }
-  
-chrome.runtime.sendMessage({type: "currDataReq"}, processData);
