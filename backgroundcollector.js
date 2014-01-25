cdbfc588681e007c8a4df40a128b0d6ec55799c0
Thomas Teisberg <teisberg@stanford.edu>
2014-01-25 00:19:46 -0800

    Updated the background page to launch a popup with the current tab every 30 seconds.
    Removed old manifest_version=1 from the manifest.json file

--- backgroundcollector.js
+++ backgroundcollector.js
@@ -1,1 +1,12 @@
 alert("background page loaded");
+
+recordPage = function (tabs) {
+	alert(tabs[0].url);
+	setTimeout(getPage, 30000);
+}
+
+getPage = function () {
+	chrome.tabs.query({"active": true, "lastFocusedWindow": true}, recordPage);
+}
+
+getPage();
