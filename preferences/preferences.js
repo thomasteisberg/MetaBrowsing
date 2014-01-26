4f41d7b12466e1bf792c00bb6f07f418bd324165
Alex Bertrand <alex.bertrand13@gmail.com>
2014-01-26 02:20:21 -0800

    I'm half asleep.

---
+++ preferences/preferences.js
@@ -0,0 +1,15 @@
+$("#emailSelection").submit(function(){
+	dropdown = document.getElementById("dropdown");
+	chrome.storage.local.set({'email': true}, function() {
+		document.getElementById('alerttext').innerHTML = "Your preferences have been saved!<br>";
+		document.getElementById('homelink').innerHTML = "Click <a href='../visualization/index.html'>here</a> to return home!";
+	});
+	return false;
+});
+
+/*function checkValue()
+{
+	chrome.storage.local.get('email', function (items) {
+			console.log(items['email']);
+	});
+}*/
