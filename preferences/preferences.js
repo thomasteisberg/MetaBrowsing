ddb6a9dc6676345df1be5e27a2a313fdf423e3db
Alex Bertrand <alex.bertrand13@gmail.com>
2014-01-26 02:53:04 -0800

    Push push push.

--- preferences/preferences.js
+++ preferences/preferences.js
@@ -1,8 +1,10 @@
 $("#emailSelection").submit(function(){
 	dropdown = document.getElementById("dropdown");
-	chrome.storage.local.set({'email': true}, function() {
-		document.getElementById('alerttext').innerHTML = "Your preferences have been saved!<br>";
-		document.getElementById('homelink').innerHTML = "Click <a href='../visualization/index.html'>here</a> to return home!";
+	chrome.storage.local.set({'emailBool': dropdown.value}, function() {
+		chrome.storage.local.set({'email': email.value}, function () {
+			document.getElementById('alerttext').innerHTML = "Your preferences have been saved!<br>";
+			document.getElementById('homelink').innerHTML = "Click <a href='../visualization/index.html'>here</a> to return home!";
+		});
 	});
 	return false;
 });
